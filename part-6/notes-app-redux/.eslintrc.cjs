c0df5f42d5abcb353ccbe536408427713099fecb
iosebkhe <ioseb.khetsuriani@yahoo.com>
2024-01-19 23:25:17 +0400

    first part of part  - 6 finished

--- part-6/notes-app-redux/.eslintrc.cjs
+++ part-6/notes-app-redux/.eslintrc.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true, es2020: true, "jest/globals": true
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -17,4 +19,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
--- part-6/notes-app-redux/src/main.jsx
+++ part-6/notes-app-redux/src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+
+if (import.meta.hot) {
+  import.meta.hot.on(
+    "vite:beforeUpdate",
+    () => console.clear()
+  );
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
